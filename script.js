b6d811721340b4dfd3e284cee813f1adaa704256
JoshDT900 <DJTroutman900@gmail.com>
2022-06-12 09:30:34 -0400

    Create basic math functions

---
+++ script.js
@@ -0,0 +1,29 @@
+// Math Functions
+
+let addFunc = (a, b) => {
+  let val1 = a;
+  let val2 = b;
+
+  return val1 + val2
+}
+
+let subtrFunc = (a, b) => {
+  let val1 = a;
+  let val2 = b;
+
+  return val1 - val2;
+}
+
+let multiFunc = (a, b) => {
+  let val1 = a;
+  let val2 = b;
+
+  return val1 * val2;
+}
+
+let diviFunc = (a, b) => {
+  let val1 = a;
+  let val2 = b;
+
+  return val1 / val2;
+}
