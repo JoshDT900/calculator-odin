f149df72d3b839d010b1a26481d810ec285b62da
JoshDT900 <DJTroutman900@gmail.com>
2022-06-12 11:06:09 -0400

    Setup GUI for calc and add functionality

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+let buttons = document.querySelectorAll(".btn")
+let output = document.querySelector(".calcOutput")
+let numArr = [];
+let calcArr = [];
+
+let logFunc = (e) => {
+  numArr.push(e.target.textContent)
+  output.textContent = numArr.join("")
+
+  if (e.target.textContent === "+" || e.target.textContent === "-" || e.target.textContent === "*" || e.target.textContent === "/") {
+    calcArr[0] = numArr.join("").toString()
+  }
+  console.log(numArr);
+  console.log(calcArr);
+}
+
+buttons.forEach(btn => btn.addEventListener('click', logFunc))
+
+
+
 // Math Functions
 
 let addFunc = (a, b) => {
@@ -27,3 +47,18 @@ let diviFunc = (a, b) => {
 
   return val1 / val2;
 }
+
+let operate = (oper, a, b) => {
+  let opVal = oper;
+
+  if (opVal === "+") {
+    return addFunc(a, b);
+  } else if (opVal === "*") {
+    return multiFunc(a, b);
+  } else if (opVal === "-") {
+    return subtrFunc(a, b);
+  } else if (opVal === "/") {
+    return diviFunc(a, b);
+  }
+}
+
