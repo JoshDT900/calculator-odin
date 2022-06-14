aae43525f7f3e2e5fe7abfcbd654fa1cb719ea09
JoshDT900 <DJTroutman900@gmail.com>
2022-06-14 19:01:06 -0400

    Add working functionality

--- script.js
+++ script.js
@@ -2,72 +2,26 @@ let buttons = document.querySelectorAll(".btn")
 let output = document.querySelector(".calcOutput")
 let outputTwo = document.querySelector(".calcOutputTwo")
 let clearBtn = document.querySelector(".clearBtn")
-let addBtn = document.querySelector("#btnAdd")
 let equalsBtn = document.querySelector("#btnEqual")
+let addBtn = document.querySelector("#btnAdd")
 let subBtn = document.querySelector("#btnSub")
 let multBtn = document.querySelector("#btnMulti")
 let divBtn = document.querySelector("#btnDiv")
 let total = 0;
-let oper = '';
+let oper = [];
 let numArr = [];
 let tempNum = '';
 
-let mathFunc = (e) => {
-  if ()
-
-
-  let val = e.target.textContent;  
-
-  if (val === "+" || val === "/" || val === "*" || val === "-") {  
-    oper = val;
-    numArr.push(tempNum);
-    tempNum = '';
-    console.log(`The oper is: ${oper}`);   
-  } else if (val === "=") {
-    numArr.push(tempNum);
-    total = operate(oper, numArr);
-    total = total.toFixed(2);
-    console.log(`The total is: ${total}`);
-  } else if (typeof parseInt(val) === 'number' || val === ".") {
-    tempNum += val;
-  }
-  
-  console.log(`The oper is: ${oper}`);
-  console.log(`The total is: ${total}`);
-  console.log(`The numArr is: ${numArr}`);
-  console.log(`The tempNum is: ${tempNum}`);
-}
-
-let clearAll = () => {
-  output.textContent = "";
-  outputTwo.textContent = "";
-  total = [];
-  numArr = [];
-  oper = '';
-  tempNum = '';
-}
-
-let clearVals = () => {
-  total = [];
-  oper = '';
-  numArr = [];
-  tempNum = '';
-}
-
-buttons.forEach(btn => btn.addEventListener('click', mathFunc))
-equalsBtn.addEventListener('click', clearVals)
-clearBtn.addEventListener('click', clearAll)
-
 let addFunc = (arr) => {
-  return arr.reduce((prevVal, currVal) => parseFloat(prevVal) + parseFloat(currVal))
+  return arr.reduce((prevVal, currVal) => parseFloat(prevVal) + parseFloat(currVal));
 }
 
 let subtrFunc = (arr) => {
-  return arr.reduce((prevVal, currVal) => parseFloat(prevVal) - parseFloat(currVal))
+  return arr.reduce((prevVal, currVal) => parseFloat(prevVal) - parseFloat(currVal));
 }
 
 let multiFunc = (arr) => {
-  return arr.reduce((prevVal, currVal) => parseFloat(prevVal) * parseFloat(currVal))
+  return arr.reduce((prevVal, currVal) => parseFloat(prevVal) * parseFloat(currVal));
 }
 
 let diviFunc = (arr) => {
@@ -87,3 +41,73 @@ let operate = (oper, arr) => {
     return diviFunc(arr);
   }
 }
+
+let symbFunc = (e) => {
+  let operHolder = e.target.textContent;
+  oper.push(operHolder);
+  numArr.push(tempNum);
+  tempNum = '';  
+  total = operate(oper[0], numArr);
+  numArr = [];
+  numArr.push(total);
+
+  output.textContent = total;
+  console.log(`The opertor is: ${oper}`);
+  console.log(`The total is: ${total}`);
+  console.log(`The number array is: ${numArr}`);
+  console.log(`The temp number is: ${tempNum}`);
+}
+
+let mathFunc = (e) => {
+  let val = e.target.textContent;
+
+  if (parseInt(val) >= 0 || parseInt(val) <= 0 || val === '.') {
+    tempNum += val;
+  } else if (val === "=") {
+    numArr.push(tempNum);
+    total = operate(oper[0], numArr);
+    total = total.toFixed(2);
+    output.textContent = total;
+  }
+
+  if (oper.length >= 2) {
+    oper.shift();
+  }
+  
+  if (numArr[0] != undefined) {
+    outputTwo.textContent = `${numArr[0]} ${oper[0]} ${tempNum}`;
+  } else {
+    outputTwo.textContent = `${tempNum}`
+  }
+  
+  console.log(`The opertor is: ${oper}`);
+  console.log(`The total is: ${total}`);
+  console.log(`The number array is: ${numArr}`);
+  console.log(`The temp number is: ${tempNum}`);
+}
+
+let clearAll = () => {
+  output.textContent = "";
+  outputTwo.textContent = "";
+  total = [];
+  numArr = [];
+  oper = '';
+  tempNum = '';
+}
+
+let clearVals = () => {
+  total = [];
+  oper = '';
+  numArr = [];
+  tempNum = '';
+}
+
+buttons.forEach(btn => btn.addEventListener('click', mathFunc));
+buttons.forEach(btn => btn.addEventListener(''))
+addBtn.addEventListener('click', symbFunc);
+subBtn.addEventListener('click', symbFunc);
+divBtn.addEventListener('click', symbFunc);
+multBtn.addEventListener('click', symbFunc);
+equalsBtn.addEventListener('click', clearVals);
+clearBtn.addEventListener('click', clearAll);
+
